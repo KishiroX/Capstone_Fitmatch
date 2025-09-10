20a6e91fc697610b1389827f0dc89195ffb2fc03
Joshua Castro <castro.277027@malolos.sti.edu.ph>
2025-09-10 21:32:59 +0800

    joshua

--- app/src/main/java/com/example/capstone/ResultScreen.kt
+++ app/src/main/java/com/example/capstone/ResultScreen.kt
@@ -34,7 +34,7 @@ fun ResultScreen(navController: NavController) {
     }
 
     val resultData = remember { mutableStateOf<Map<String, Any>>(emptyMap()) }
-//lalalala
+//lala
     LaunchedEffect(savedBitmap) {
         savedBitmap?.let { bitmap ->
             val bodyData = withContext(Dispatchers.Default) {
